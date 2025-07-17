230ad663ebe9602a3c7a536381618ff25b81b553
umangadukia <umangadukia@gmail.com>
2025-07-17 15:37:01 +0800

    updated the add numbers to add 3 numbers

--- add_numbers.py
+++ add_numbers.py
@@ -1,7 +1,8 @@
 # Example: Function to add two numbers
-def add_numbers(a, b):
-return a + b
+def add_numbers(a, b, c):
+return a + b + c
 
 num1 = 15
 num2 = 7
+num3 = 10
 print("The sum is:", add_numbers(num1, num2))
